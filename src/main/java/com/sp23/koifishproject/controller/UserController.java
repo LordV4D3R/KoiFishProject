47b528bc61d10614344a8063300e98bdebcf2542
Vader <tranquocan1508@gmail.com>
2024-10-05 18:01:39 +0700

    feat: Add Swagger

--- src/main/java/com/sp23/koifishproject/controller/UserController.java
+++ src/main/java/com/sp23/koifishproject/controller/UserController.java
@@ -1,7 +1,7 @@
 package com.sp23.koifishproject.controller;
 
 import com.sp23.koifishproject.model.User;
-import com.sp23.koifishproject.service.UserService;
+import com.sp23.koifishproject.service.IService.IUserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,7 +14,7 @@ import java.util.List;
 public class UserController {
 
     @Autowired
-    private UserService userService;
+    private IUserService userService;
 
     //Get all users
     @GetMapping
